1b28e4dfb0027229f8ac3e7df704e416e244373e
Gkostov01 <georgi.kostov0101@gmail.com>
2025-11-16 13:16:36 +0200

    add invulnerable state after player destruction

--- RainGame/include/Player/PlayerSpaceShip.h
+++ RainGame/include/Player/PlayerSpaceShip.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "SpaceShip/Spaceship.h"
+#include "framework/TimerManager.h"
 
 namespace rn
 {
@@ -11,12 +12,13 @@ namespace rn
 	public:
 		PlayerSpaceship(World* owner, const std::string& path = "SpaceShooterRedux/PNG/playerShip1_blue.png");
 
+		virtual void BeginPlay() override;
 		virtual void Tick(float deltaTime) override;
+		virtual void ApplyDamage(float amount) override;
+		virtual void Shoot() override;
 
 		void SetSpeed(float speed) { _speed = speed; }
 		float GetSpeed() const { return _speed; }
-
-		virtual void Shoot() override;
 
 		void SetShooter(unique<Shooter>&& newShooter);
 
@@ -26,9 +28,21 @@ namespace rn
 		void NormalizeInput();
 		void ClampInputOnEdge();
 
+		void StopInvulnerable();
+		void UpdateInvulnerable(float deltaTime);
+
 	private:
 		sf::Vector2f _moveInput;
 		float _speed;
+
+		float _invulnerableTime;
+		float _invulnerableFlashInterval;
+		float _invulnerableFlashTimer;
+		float _invulnerableFlashDirection; // Ping pong the color for a flashing effect
+
+		bool _isInvulnerable;
+
+		TimerHandle _invulnerableTimer;
 
 		unique<Shooter> _shooter;
 	};
--- RainGame/src/Player/PlayerSpaceShip.cpp
+++ RainGame/src/Player/PlayerSpaceShip.cpp
@@ -11,9 +11,20 @@ namespace rn
 		: Spaceship{ owner, path },
 		_moveInput{},
 		_speed{ 400.0f },
+		_invulnerableTime{2.0f},
+		_invulnerableFlashInterval{0.3f},
+		_invulnerableFlashTimer{0.0f},
+		_invulnerableFlashDirection{1.0f},
+		_isInvulnerable{true},
 		_shooter{ new BulletShooter{this, 0.15f, {50.0f, 0.0f}} }
 	{
 		SetTeamID(1);
+	}
+
+	void PlayerSpaceship::BeginPlay()
+	{
+		Spaceship::BeginPlay();
+		TimerManager::Get().SetTimer(GetWeakReference(), &PlayerSpaceship::StopInvulnerable, _invulnerableTime);
 	}
 
 	void PlayerSpaceship::Tick(float deltaTime)
@@ -21,6 +32,15 @@ namespace rn
 		Spaceship::Tick(deltaTime);
 		HandleInput();
 		ConsumeInput(deltaTime);
+		UpdateInvulnerable(deltaTime);
+	}
+
+	void PlayerSpaceship::ApplyDamage(float amount)
+	{
+		if (!_isInvulnerable)
+		{
+			Spaceship::ApplyDamage(amount);
+		}
 	}
 
 	void PlayerSpaceship::Shoot()
@@ -91,5 +111,26 @@ namespace rn
 		if (actorLocation.x > GetWindowSize().x && _moveInput.x == 1)	{ _moveInput.x = 0; }			// Right
 		if (actorLocation.y < 0 && _moveInput.y == -1)					{ _moveInput.y = 0; }			// Up
 		if (actorLocation.y > GetWindowSize().y && _moveInput.y == 1)	{ _moveInput.y = 0; }			// Down
+	}
+
+	void PlayerSpaceship::StopInvulnerable()
+	{
+		GetSprite().setColor({ 255, 255, 255, 255 });
+		_isInvulnerable = false;
+	}
+
+	void PlayerSpaceship::UpdateInvulnerable(float deltaTime)
+	{
+		if (!_isInvulnerable)
+			return;
+
+		_invulnerableFlashTimer += deltaTime * _invulnerableFlashDirection;
+
+		if (_invulnerableFlashTimer < 0 || _invulnerableFlashTimer > _invulnerableFlashInterval)
+		{
+			_invulnerableFlashDirection *= -1;
+		}
+
+		GetSprite().setColor(LerpColor({ 255, 255, 255, 64 }, {255, 255, 255, 128}, _invulnerableFlashTimer / _invulnerableFlashInterval));
 	}
 }
